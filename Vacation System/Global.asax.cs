d7094b6726f53c730c709a0e3c14d3fdb43ec11e
AleKiller21 <aleferrera@hotmail.com>
2015-09-01 09:55:04 -0600

    Error 404 implemented

--- Vacation System/Global.asax.cs
+++ Vacation System/Global.asax.cs
@@ -13,6 +13,7 @@ namespace Vacation_System
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalFilters.Filters.Add(new HandleErrorAttribute());
         }
     }
 }
